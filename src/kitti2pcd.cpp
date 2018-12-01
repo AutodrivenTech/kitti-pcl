b345351955633e27c17ea1125c9ba631769b8410
jdze <jdze@hsns.cc>
2018-12-01 17:42:17 +0800

    Add directories tranlaste

--- src/kitti2pcd.cpp
+++ src/kitti2pcd.cpp
@@ -16,69 +16,101 @@
 
 #include <iostream>
 #include <fstream>
+#include <dirent.h>
+#include <cstring>
 
 using namespace pcl;
 using namespace std;
 
-namespace po = boost::program_options;
+//namespace po = boost::program_options;
 
-int main(int argc, char **argv){
-	if(argc != 5){
-		cout << "You need to use follow args:" << endl;
-		cout << "--infile" << "\t" << "the file to read a point cloud from" << endl;
-		cout << "--outfile" << "\t" << "the file to write the DoN point cloud & normals to" << endl;
-		return 0;
-	}
-	///The file to read from.
-	string infile;
 
-	///The file to output to.
-	string outfile;
+void kitti2pcd(string infile, string outfile){
+    fstream input(infile, ios::in | ios::binary);
+    if(!input.good()){
+        cerr << "Could not read file: " << infile << endl;
+        exit(EXIT_FAILURE);
+    }
+    input.seekg(0, ios::beg);
 
-	// Declare the supported options.
-	po::options_description desc("Program options");
-	desc.add_options()
-		//Options
-		("infile", po::value<string>(&infile)->required(), "the file to read a point cloud from")
-		("outfile", po::value<string>(&outfile)->required(), "the file to write the DoN point cloud & normals to")
-		;
-	// Parse the command line
-	po::variables_map vm;
-	po::store(po::parse_command_line(argc, argv, desc), vm);
+    pcl::PointCloud<PointXYZI>::Ptr points (new pcl::PointCloud<PointXYZI>);
 
-	// Print help
-	if (vm.count("help"))
-	{
-		cout << desc << "\n";
-		return false;
-	}
+    int i;
+    for (i=0; input.good() && !input.eof(); i++) {
+        PointXYZI point;
+        input.read((char *) &point.x, 3*sizeof(float));
+        input.read((char *) &point.intensity, sizeof(float));
+        points->push_back(point);
+    }
+    input.close();
 
-	// Process options.
-	po::notify(vm);
-
-	// load point cloud
-	fstream input(infile.c_str(), ios::in | ios::binary);
-	if(!input.good()){
-		cerr << "Could not read file: " << infile << endl;
-		exit(EXIT_FAILURE);
-	}
-	input.seekg(0, ios::beg);
-
-	pcl::PointCloud<PointXYZI>::Ptr points (new pcl::PointCloud<PointXYZI>);
-
-	int i;
-	for (i=0; input.good() && !input.eof(); i++) {
-		PointXYZI point;
-		input.read((char *) &point.x, 3*sizeof(float));
-		input.read((char *) &point.intensity, sizeof(float));
-		points->push_back(point);
-	}
-	input.close();
-
-	cout << "Read KTTI point cloud with " << i << " points, writing to " << outfile << endl;
+    cout << "Read KTTI point cloud with " << i << " points, writing to " << outfile << endl;
 
     pcl::PCDWriter writer;
 
     // Save DoN features
     writer.write<PointXYZI> (outfile, *points, false);
 }
+
+
+int main(int argc, char **argv){
+	if(argc != 5){
+		cout << "There are two type of usage:" << endl;
+		cout << "\t" << "./kitti2pcd --infile <infile path> --outfile <outfile path>"  << endl;
+		cout << "\t\t" <<"--infile" << "\t" << "the file to read a point cloud from" << endl;
+		cout << "\t\t"  << "--outfile" << "\t" << "the file to write the DoN point cloud & normals to" << endl;
+        cout << "\t" << "./kitti2pcd --indir <indir path> --outfile <outdir path>"  << endl;
+        cout << "\t\t" <<"--indir" << "\t" << "the dir to read a point cloud from" << endl;
+        cout << "\t\t"  << "--outdir" << "\t" << "the dir to write the DoN point cloud & normals to" << endl;
+		return 0;
+	}
+
+	if(strcmp(argv[1], "--infile") == 0 && strcmp(argv[3], "--outfile") == 0){
+	    string infile(argv[2]);
+	    string outfile(argv[4]);
+	    kitti2pcd(infile, outfile);
+	    return 0;
+	}
+	if(strcmp(argv[1], "--indir") == 0 && strcmp(argv[3], "--outdir") == 0){
+//	    cout << "Test" << endl;
+        DIR *dir;
+        struct dirent *ent;
+        if ((dir = opendir (argv[2])) != NULL) {
+            string indir(argv[2]);
+            string outdir(argv[4]);
+            /* print all the files and directories within directory */
+            while ((ent = readdir (dir)) != NULL ) {
+                if(ent->d_type != DT_DIR){
+                    string name(ent->d_name);
+                    string postfix = name.substr(name.length()-4);
+                    string prefix = name.substr(0, name.length() -4);
+//                    cout << name  << ":" << postfix << ":" << prefix << "\n";
+                    if(postfix == ".bin"){
+                        string outfile = outdir + "/" + prefix + ".pcd";
+                        string infile = indir  + "/" + name;
+//                        cout << infile << ":\n\t" << outfile << "\n";
+                        kitti2pcd(infile.c_str(), outfile.c_str());
+                    }
+                }
+
+            }
+            closedir (dir);
+            return 0;
+        } else {
+            /* could not open directory */
+            perror ("can't open directory");
+            return 1;
+        }
+	}
+
+    cout << "There are two type of usage:" << endl;
+    cout << "\t" << "./kitti2pcd --infile <infile path> --outfile <outfile path>"  << endl;
+    cout << "\t\t" <<"--infile" << "\t" << "the file to read a point cloud from" << endl;
+    cout << "\t\t"  << "--outfile" << "\t" << "the file to write the DoN point cloud & normals to" << endl;
+    cout << "\t" << "./kitti2pcd --indir <indir path> --outfile <outdir path>"  << endl;
+    cout << "\t\t" <<"--indir" << "\t" << "the dir to read a point cloud from" << endl;
+    cout << "\t\t"  << "--outdir" << "\t" << "the dir to write the DoN point cloud & normals to" << endl;
+    return 0;
+
+}
+
